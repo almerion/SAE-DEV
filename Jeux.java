631dab6355c403e5c4155bfa8481782e7c41de0f
almerion <tig2468.a@gmail.com>
2021-12-17 14:23:13 +0100

    bon y'a le cancer un peu

--- Jeux.java
+++ Jeux.java
@@ -3,11 +3,16 @@ public class Jeux {
 
 
     char[][] t= new char[10][10];
-    PremierNiveau.niveaux(t);
+    Niveaux.niveaux(t);
     MéthodePrincipale.afficherTableau(t);
     int[] coordonnees = {0,0};
     MéthodePrincipale.ChercherPerso(t,coordonnees);
     System.out.println(coordonnees[0]);
     System.out.println(coordonnees[1]);
+
+    while (true){
+        Menu.mouvement(t,coordonnees);
+        MéthodePrincipale.afficherTableau(t);
+    }
     }
 }
