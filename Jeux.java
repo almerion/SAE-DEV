a397d9832b6a117d1851426f39c8ca83bc85f27a
almerion <tig2468.a@gmail.com>
2021-12-26 19:45:51 +0100

    déplacement des while dans le menu

--- Jeux.java
+++ Jeux.java
@@ -1,18 +1,5 @@
 public class Jeux {
     public static void main(String[] args) {
-
-
-        char[][] t = new char[10][10];
-        int[] coordonnees = {0, 0};
-        Niveaux.CopieNiveau(t,1, coordonnees);
-        MethodePrincipale.afficherTableau(t);
-        System.out.println(coordonnees[0]);
-        System.out.println(coordonnees[1]);
-
-        while (true) {
-            Menu.mouvement(t, coordonnees);
-            MethodePrincipale.afficherTableau(t);
-        }
-        // TODO : while dans le while pour changer de niveau ( while tours > 0 while !victoire) et entre les deux victoire qui est reset et changement de niveau
+        Menu.menu();
     }
 }
--- Menu.java
+++ Menu.java
@@ -1,40 +1,53 @@
 import java.util.Scanner;
 
 public class Menu {
-    public static void mouvement(char[][] niveau,int [] coordonnees){
-        char direction;
+    public static void menu(){
+        char[][] niveau = new char[10][10];
+        int[] coordonnees = new int[2];
+        char direction = 'b';
+        int numNiveau = 1;
+        int tours;
+        boolean victoire = false;
         int[] ProchaineCase = new int[2];
+        Scanner sc = new Scanner(System.in);
         System.arraycopy(coordonnees,0,ProchaineCase,0,2);
 
 
+        // TODO: ajouter dans le while un "&& numNiv < Le max num niv " quand tous les lvl seront faits
+        while (direction != ' ') {
+            tours = Niveaux.CopieNiveau(niveau,numNiveau,coordonnees);
+            while (tours > 0 && !victoire && direction != ' ') {
+                MethodePrincipale.afficherTableau(niveau);
+                System.out.println("Nombre de tours restants : " + tours);
+                do {
+                    direction = sc.nextLine().charAt(0);
 
-        // TODO : toutes les variables déclarées à foutre avant les while
+                    System.out.println();
 
-        Scanner sc =new Scanner(System.in);
-        do {
-            direction = sc.nextLine().charAt(0);
+                    while (direction != 'z' && direction != 'q' && direction != 's' && direction != 'd' && direction != 'Z' && direction != 'Q' && direction != 'S' && direction != 'D' && direction != ' ') {
 
-            System.out.println();
+                        System.out.println("Mauvaise commande choisie, il faut tapez z pour monter, q pour aller à gauche, s pour descendre, d pour aller à droite");
 
-            while (direction != 'z' && direction != 'q' && direction != 's' && direction != 'd' && direction != 'Z' && direction != 'Q' && direction != 'S' && direction != 'D') {
+                        direction = sc.nextLine().charAt(0);
 
-                System.out.println("Mauvaise commande choisie, il faut tapez z pour monter, q pour aller à gauche, s pour descendre, d pour aller à droite");
+                    }
+                    // Regarde si l'endroit ou le joueur veut se déplacer est bloqué par un mur/ par la bordure ou non
+                    if (MethodePrincipale.PresenceMurOuBordure(niveau, direction, coordonnees))
+                        System.out.println("pas possible de bouger à cet endroit");
+                }
 
-                direction = sc.nextLine().charAt(0);
-
+                while (MethodePrincipale.PresenceMurOuBordure(niveau, direction, coordonnees));
+                if (MethodePrincipale.VerifVictoire(niveau, direction, coordonnees)) {
+                    System.out.println("GG je suppose");
+                    victoire = true;
+                    numNiveau ++;
+                }
+                else
+                    MethodePrincipale.Mouvement(niveau, direction, coordonnees);
+                tours --;
             }
-            // Regarde si l'endroit ou le joueur veut se déplacer est bloqué par un mur/ par la bordure ou non
-            if (MethodePrincipale.PresenceMurOuBordure(niveau,direction,coordonnees))
-                System.out.println("pas possible de bouger à cet endroit");
+
+
         }
-        // TODO : pas juste présence mais si caillou ou bordure et traiter les pierres à casser/déplacer
-        while (MethodePrincipale.PresenceMurOuBordure(niveau,direction,coordonnees));
-        if (MethodePrincipale.VerifVictoire(niveau,direction,coordonnees))
-            System.out.println("GG je suppose");
-        // TODO variable boolean victoire
-        else
-            MethodePrincipale.Mouvement(niveau,direction,coordonnees);
-
-
     }
 }
